3554038592ef4df3798136e8cf85c38cd0f162f3
PooyaKhalili <pooyakhpc@gmail.com>
2026-06-06 21:45:21 +0330

    add ProductRepository with CRUD, search, stock management and JSON persistence

--- src/main/java/com/storesystem/model/Product.java
+++ src/main/java/com/storesystem/model/Product.java
@@ -1,4 +1,32 @@
 package com.storesystem.model;
 
 public class Product {
+    private long code;
+    private String name;
+    private double price;
+    private int categoryId;
+    private int stock;
+    public Product() {
+        this.code = 0;
+    }
+    public Product(String name, long price, int stock, int categoryId) {
+        this.code = 0;
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+        this.categoryId = categoryId;
+    }
+    public long getCode() {return code;}
+    public void setCode(long code) {
+        this.code = code;
+    }
+    public String getName() {return name;}
+    public void setName(String name) {this.name = name;}
+    public double getPrice() {return price;}
+    public void setPrice(double price) {this.price = price;}
+    public int getCategoryId() {return categoryId;}
+    public void setCategoryId(int categoryId) {this.categoryId = categoryId;}
+    public int getStock() {return stock;}
+    public void setStock(int stock) {this.stock = stock;}
+
 }
